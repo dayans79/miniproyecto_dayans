77004c32912e61e492f0e8ba3f3818b660e2571a
Dayans <84407238+dayans79@users.noreply.github.com>
2021-06-17 13:51:32 -0500

    Create DatosModel.php

---
+++ codeingniter/app/Models/DatosModel.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Models;
+use codeigniter\Model;
+class DatosModel extends Model{
+    public  function obtenerinformacion($data){
+      $gModel = $this->db->table('persona');
+      $gModel->where($data);
+      return $gModel->get()->getResultArray();
+      
+      
+    }
+  
+  public function listartodo(){
+    $gModel = $this->db->query("SELECT + FROM persona");
+      return $gModel->getResult();
+      
+      
+  }
+    
+    
+    public function insertar($data){
+        $gModel = $this->db->table('persona');
+        $gModel->insert($data);
+        return $this->db->insertID();
+        
+        
+        
+    } 
+    
+    
+    
+    public function actualizar ($data,$id){
+        $gModel = $this->db->table('persona');
+        $gModel->set($data);
+        $gModel->where($id);
+        return $gModel->update();
+        
+        
+    }
+    
+    
+    
+    public function  eliminar($id){
+        $gModel = $this->db->table('persona');
+        $gModel = $this->where($id);
+        return $gModel->delete();
+        
+        
+    }
+    
+    
+    
+}
+uu.txt
+2 KB
